ce45fbbfa787e11e669a7c2410c9981887704818
KOVALcraft <68643916+KOVALcraft@users.noreply.github.com>
2020-07-22 15:09:29 +0300

    Проверка стартовой страницы
    Добавление прослушивания стартовой страницы с сообщением-заглушкой

--- index.js
+++ index.js
@@ -2,5 +2,8 @@ const express = require('http');
 const app = express();
 
 const port = 3004;
-
+app.get('/', async(req,res)=>{
+  res.write('home page');
+  res.end();
+})
 app.listen(port, ()=>{console.log('server started')});
